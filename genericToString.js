bd4c8ecd3bc219e6a6c806d64d1ddf0053efaa5a
kremlevmax <kremlevmax1989@gmail.com>
2022-10-15 22:36:34 -0700

    Generic toString

---
+++ genericToString.js
@@ -0,0 +1,14 @@
+"use strict";
+function genericToString(input) {
+    if (typeof input === "object" && !Array.isArray(input) && input !== null) {
+        return JSON.stringify(input);
+    }
+    else {
+        return String(input);
+    }
+}
+console.log(genericToString([1, 2, 3]));
+console.log(genericToString(1));
+console.log(genericToString({ name: "Alex", age: 12 }));
+console.log(genericToString(true));
+console.log(genericToString(undefined));
