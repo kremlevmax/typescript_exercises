33c0325e336bc051753a2f84f63ec3acd16da328
kremlevmax <kremlevmax1989@gmail.com>
2022-10-17 22:39:31 -0700

    Intersection Type with Omit<Type, Parameter>

--- user.ts
+++ user.ts
@@ -12,7 +12,10 @@ interface Admin {
   role: string;
 }
 
-export type Person = User | Admin;
+type PowerUser = { type: "powerUser" } & Omit<Admin, "type"> &
+  Omit<User, "type">;
+
+export type Person = User | Admin | PowerUser;
 
 export const persons: Person[] = [
   {
@@ -21,68 +24,53 @@ export const persons: Person[] = [
     age: 25,
     occupation: "Chimney sweep",
   },
+  { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
+  { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
+  { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
   {
-    type: "admin",
-    name: "Jane Doe",
-    age: 32,
-    role: "Administrator",
-  },
-  {
-    type: "user",
-    name: "Kate Müller",
-    age: 23,
-    occupation: "Astronaut",
-  },
-  {
-    type: "admin",
-    name: "Bruce Willis",
-    age: 64,
-    role: "World saver",
-  },
-  {
-    type: "user",
-    name: "Wilson",
-    age: 23,
-    occupation: "Ball",
-  },
-  {
-    type: "admin",
-    name: "Agent Smith",
-    age: 23,
-    role: "Administrator",
+    type: "powerUser",
+    name: "Nikki Stone",
+    age: 45,
+    role: "Moderator",
+    occupation: "Cat groomer",
   },
 ];
 
-export const isAdmin = (person: Person): person is Admin =>
-  person.type === "admin";
-export const isUser = (person: Person): person is User =>
-  person.type === "user";
+function isAdmin(person: Person): person is Admin {
+  return person.type === "admin";
+}
+
+function isUser(person: Person): person is User {
+  return person.type === "user";
+}
+
+function isPowerUser(person: Person): person is PowerUser {
+  return person.type === "powerUser";
+}
 
 export function logPerson(person: Person) {
-  let additionalInformation = "";
+  let additionalInformation: string = "";
   if (isAdmin(person)) {
     additionalInformation = person.role;
   }
   if (isUser(person)) {
     additionalInformation = person.occupation;
   }
-  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+  if (isPowerUser(person)) {
+    additionalInformation = `${person.role}, ${person.occupation}`;
+  }
+  console.log(`${person.name}, ${person.age}, ${additionalInformation}`);
 }
 
-export function filterUsers<T>(
-  persons: Person[],
-  criteria: Partial<User>
-): User[] {
-  return persons.filter(isUser).filter((user) => {
-    const criteriaKeys = Object.keys(criteria) as (keyof User)[];
-    return criteriaKeys.every((fieldName) => {
-      return user[fieldName] === criteria[fieldName];
-    });
-  });
-}
+console.log("Admins:");
+persons.filter(isAdmin).forEach(logPerson);
 
-console.log("Users of age 23:");
+console.log();
 
-filterUsers(persons, {
-  age: 23,
-}).forEach(logPerson);
+console.log("Users:");
+persons.filter(isUser).forEach(logPerson);
+
+console.log();
+
+console.log("Power users:");
+persons.filter(isPowerUser).forEach(logPerson);
