c6ff537090bd07c659c95cbc72dbc5db199bc2a9
kremlevmax <kremlevmax1989@gmail.com>
2022-10-17 18:23:30 -0700

    person is User type guard

--- user.ts
+++ user.ts
@@ -1,10 +1,12 @@
 interface User {
+  type: "user";
   name: string;
   age: number;
   occupation: string;
 }
 
 interface Admin {
+  type: "admin";
   name: string;
   age: number;
   role: string;
@@ -14,35 +16,36 @@ export type Person = User | Admin;
 
 export const persons: Person[] = [
   {
+    type: "user",
     name: "Max Mustermann",
     age: 25,
     occupation: "Chimney sweep",
   },
-  {
-    name: "Jane Doe",
-    age: 32,
-    role: "Administrator",
-  },
-  {
-    name: "Kate Müller",
-    age: 23,
-    occupation: "Astronaut",
-  },
-  {
-    name: "Bruce Willis",
-    age: 64,
-    role: "World saver",
-  },
+  { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
+  { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
+  { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
 ];
 
-export function logPerson(person: Person) {
-  let additionalInformation: string;
-  if ("role" in person) {
-    additionalInformation = person.role;
-  } else {
-    additionalInformation = person.occupation;
-  }
-  console.log(person.name, person.age, additionalInformation);
+export function isAdmin(person: Person): person is Admin {
+  return person.type === "admin";
 }
 
-persons.forEach(logPerson);
+export function isUser(person: Person): person is User {
+  return person.type === "user";
+}
+
+export function logPerson(person: Person) {
+  let additionalInformation: string = "";
+  if (isAdmin(person)) {
+    additionalInformation = person.role;
+  }
+  if (isUser(person)) {
+    additionalInformation = person.occupation;
+  }
+  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+console.log("Admins:");
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log();
